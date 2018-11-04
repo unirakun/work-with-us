f45707a6baaf1e90e67f735c76302da7b3efb4f4
Fabien JUIF <fabien.juif@gmail.com>
2018-11-04 15:07:09 +0100

    :sparkles: summary

--- packages/cv-to-pdf/index.js
+++ packages/cv-to-pdf/index.js
@@ -3,7 +3,10 @@ const puppeteer = require('puppeteer')
 const run = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('http://localhost:4000')
+  await page.goto('http://localhost:3000')
+  await page.evaluate(() => {
+    root.style.fontSize = "12px";
+  })
   await page.pdf({ path: 'cv.pdf', format: 'A4', printBackground: true })
 
   await browser.close()
--- packages/ui/src/cv/experiences/experiences.js
+++ packages/ui/src/cv/experiences/experiences.js
@@ -5,6 +5,36 @@ import Experience from './experience'
 const Experiences = ({ className, children }) => (
   <div className={className}>
     <h1>Expériences</h1>
+    <div className="summary">
+      <div className="shadow top" />
+      <div className="background" />
+
+      <ul>
+        {children
+          .filter((_, index) => index <= children.length / 2)
+          .map(experience => (
+            <li>
+              {experience.title}
+              <div> {(experience.client || experience.for).name}</div>
+            </li>
+          ))
+        }
+      </ul>
+
+      <ul>
+        {children
+          .filter((_, index) => !(index <= children.length / 2))
+          .map(experience => (
+            <li>
+              {experience.title}
+              <div> {(experience.client || experience.for).name}</div>
+            </li>
+          ))
+        }
+      </ul>
+      <div className="shadow bottom" />
+    </div>
+
     {children.map(experience => (
       <Experience
         key={experience.title}
@@ -16,16 +46,73 @@ const Experiences = ({ className, children }) => (
 
 export default styled(Experiences)`
   grid-area: experiences;
-  max-width: 70em;
   margin: 0 auto;
 
   & > h1 {
     text-align: center;
-    border-bottom: 1px solid ${({ theme }) => theme.Dark};
-    padding: 1em;
-    margin: 2em auto;
-    width: 15em;
-    padding-bottom: 1em;
-    margin-top: 0;
+    margin: 0 auto;
+  }
+
+  & > .summary {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin: 4em 0;
+    padding: 3em 0;
+    position: relative;
+    width: 100vw;
+
+    & > .background {
+      background-color: #f3f3f3;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 10;
+    }
+
+    & > .shadow.top {
+      box-shadow: 0 0 2em -0.4em #565656;
+      width: 70%;
+      height: 1em;
+      z-index: 0;
+      position: absolute;
+      top: 0;
+    }
+
+    & > .shadow.bottom {
+      box-shadow: 0 0 2em -0.4em #565656;
+      width: 70%;
+      height: 1em;
+      z-index: 0;
+      position: absolute;
+      bottom: 0;
+    }
+
+    & ul {
+      width: 25em;
+      list-style: none;
+      padding: 0;
+      z-index: 20;
+      margin: 0 2em;
+
+      & > li {
+        padding: 1em;
+        text-align: center;
+
+        & > div {
+          font-size: .8em;
+          color: #9a9a9a;
+          margin-top: 0.5em;
+        }
+      }
+    }
+  }
+
+  & > ${Experience} {
+    max-width: 70em;
+    margin: 0 auto;
   }
 `
