32ef6a31d1a38a371d303136ef1f1be1ffff5336
Fabien JUIF <fabien.juif@gmail.com>
2018-11-05 19:31:37 +0100

    :sparkles: ssr cache

--- packages/api-gateway/src/react.js
+++ packages/api-gateway/src/react.js
@@ -10,12 +10,21 @@ import { promisify } from 'util'
 import path from 'path'
 import fs from 'fs'
 import schema from './schema'
+import cache from './cache'
 
 const filePath = path.resolve(__dirname, '..', '..', 'ui', 'build', 'index.html')
 let htmlData
 
+const renderCache = cache({ name: 'render-cache' })
+
 module.exports = async (ctx, next) => {
   if (ctx.path === '/') {
+    const cachedItem = renderCache.get('/')
+    if (cachedItem) {
+      ctx.body = cachedItem
+      return
+    }
+
     if (!htmlData) {
       console.log('Loading html base file...')
       htmlData = await promisify(fs.readFile)(filePath, 'utf8')
@@ -39,11 +48,14 @@ module.exports = async (ctx, next) => {
     )
     const styleTags = sheet.getStyleTags()
 
-    ctx.body = htmlData
+    const replacedHtml = htmlData
       .replace('<div id="root"></div>', `<div id="root"></div><script charset="UTF-8">window.__APOLLO_STATE = ${JSON.stringify(await client.cache.extract())}</script>`)
       .replace('<div id="root"></div>', `<div id="root">${html}</div>`)
       .replace('<style data-src="server-css"></style>', styleTags)
 
+
+    renderCache.add('/', replacedHtml)
+    ctx.body = replacedHtml
   } else {
     await next()
   }
