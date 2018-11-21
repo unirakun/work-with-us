1af8d4b5f631ac36385fbbaa7c8d734638d1bf9b
Fabien JUIF <fabien.juif@gmail.com>
2018-11-21 10:45:29 +0100

    :sparkles: link from / to cv

--- packages/ui/src/app/screens/proposals/proposals.jsx
+++ packages/ui/src/app/screens/proposals/proposals.jsx
@@ -1,37 +1,13 @@
-/* eslint-disable */ /* FIXME: */
 import React from 'react'
-import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
 
-const Proposals = () => (
-  <Query
-    query={gql`
-      {
-        proposals {
-          title
-          contact {
-            email
-          }
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <div>Loading...</div>
-      if (error) {
-        console.error(error)
-        return null
-      }
-
-      const { proposals } = data
-
-      return (
-        <ul>
-          {proposals.map(({ title, contact }) => <li>{title} - ({contact.email})</li>)}
-        </ul>
-      )
-    }}
-  </Query>
+const Proposals = ({ children }) => (
+  <ul>
+    {children.map(({ title, contact }) => (
+      <li>
+        {`${title} - (${contact.email})`}
+      </li>
+    ))}
+  </ul>
 )
 
 export default Proposals
--- packages/ui/src/app/screens/proposals/screen.jsx
+++ packages/ui/src/app/screens/proposals/screen.jsx
@@ -1,12 +1,27 @@
-import React, { Fragment } from 'react'
-import Proposals from './proposals'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import Proposals from './proposals.container'
 import AddNewProposal from './addNewProposal'
 
-const ProposalScreen = () => (
-  <Fragment>
+const ProposalScreen = ({ className }) => (
+  <div className={className}>
+    <div className="links">
+      CV :
+      <NavLink to="/fabien">Fabien</NavLink>
+      <NavLink to="/guillaume">Guillaume</NavLink>
+    </div>
     <Proposals />
     <AddNewProposal />
-  </Fragment>
+  </div>
 )
 
-export default ProposalScreen
+export default styled(ProposalScreen)`
+  & > .links {
+    padding: 2em;
+
+    & > a {
+      margin: 1em;
+    }
+  }
+`
