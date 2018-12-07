460b5e94537f6b9430cbe60517871386a380ead9
Fabien JUIF <fabien.juif@gmail.com>
2018-12-07 10:05:03 +0100

    Wip directive resolver (#45)

--- packages/api-gateway/src/directives/api.js
+++ packages/api-gateway/src/directives/api.js
@@ -1,47 +1,27 @@
-/* eslint-disable no-underscore-dangle, no-param-reassign */
-// code from: https://blog.apollographql.com/reusable-graphql-schema-directives-131fb3a177d1
-import { defaultFieldResolver } from 'graphql'
-import { SchemaDirectiveVisitor } from 'graphql-tools'
+import { createVisitFieldDefinition } from 'graphql-directives-middlewares'
 
-function ensureFieldsWrapped(objectType) {
-  // Mark the GraphQLObjectType object to avoid re-wrapping:
-  if (objectType._apiFieldsWrapped) return
-  objectType._apiFieldsWrapped = true
+export default createVisitFieldDefinition(
+  'api',
+  (params, next) => async (...args) => {
+    const {
+      name: apiName,
+    } = params
 
-  const fields = objectType.getFields()
-
-  Object.keys(fields).forEach((fieldName) => {
-    const field = fields[fieldName]
-    const { resolve = defaultFieldResolver } = field
-
-    field.resolve = async (...args) => {
-      const apiName = (
-        field._apiName
-        || objectType._apiName
-      )
-
-      if (!apiName) {
-        return resolve.apply(this, args)
-      }
-
-      const [, { input }, context, definition] = args
-      const { models } = context
-      if (definition.parentType.name === 'Query') {
-        return models[apiName].list()
-      }
-      if (definition.parentType.name === 'Mutation') {
-        await models[apiName].add(input)
-        return true
-      }
-
-      return resolve.apply(this, args)
+    if (!apiName) {
+      return next()
     }
-  })
-}
 
-export default class ApiDirective extends SchemaDirectiveVisitor {
-  visitFieldDefinition(field, details) {
-    ensureFieldsWrapped(details.objectType)
-    field._apiName = this.args.name
-  }
-}
+    const [, { input }, context, definition] = args
+    const { models } = context
+
+    if (definition.parentType.name === 'Query') {
+      return models[apiName].list()
+    }
+    if (definition.parentType.name === 'Mutation') {
+      await models[apiName].add(input)
+      return true
+    }
+
+    return next()
+  },
+)
--- packages/api-gateway/src/directives/auth.js
+++ packages/api-gateway/src/directives/auth.js
@@ -1,51 +1,25 @@
-/* eslint-disable no-underscore-dangle, no-param-reassign */
-// code from: https://blog.apollographql.com/reusable-graphql-schema-directives-131fb3a177d1
-import { defaultFieldResolver, GraphQLList } from 'graphql'
-import { SchemaDirectiveVisitor } from 'graphql-tools'
+import { GraphQLList } from 'graphql'
+import { createVisitFieldDefinition } from 'graphql-directives-middlewares'
 
-function ensureFieldsWrapped(objectType) {
-  // Mark the GraphQLObjectType object to avoid re-wrapping:
-  if (objectType._authFieldsWrapped) return
-  objectType._authFieldsWrapped = true
+export default createVisitFieldDefinition(
+  'auth',
+  (params, next) => async (...args) => {
+    const {
+      requires: requiredRole,
+    } = params
 
-  const fields = objectType.getFields()
-
-  Object.keys(fields).forEach((fieldName) => {
-    const field = fields[fieldName]
-    const { resolve = defaultFieldResolver } = field
-
-    field.resolve = async (...args) => {
-      // Get the required Role from the field first, falling back
-      // to the objectType if no Role is required by the field:
-      const requiredRole = (
-        field._requiredAuthRole
-        || objectType._requiredAuthRole
-      )
-
-      if (!requiredRole) {
-        return resolve.apply(this, args)
-      }
-
-      const [, , context, definition] = args
-      const { role } = context
-      if (!role.includes(requiredRole)) {
-        if (definition.returnType instanceof GraphQLList) return []
-        throw new Error('Unauthorized')
-      }
-
-      return resolve.apply(this, args)
+    if (!requiredRole) {
+      return next()
     }
-  })
-}
 
-export default class AuthDirective extends SchemaDirectiveVisitor {
-  visitObject(type) {
-    ensureFieldsWrapped(type)
-    type._requiredAuthRole = this.args.requires
-  }
+    const [, , context, definition] = args
+    const { role } = context
 
-  visitFieldDefinition(field, details) {
-    ensureFieldsWrapped(details.objectType)
-    field._requiredAuthRole = this.args.requires
-  }
-}
+    if (!role.includes(requiredRole)) {
+      if (definition.returnType instanceof GraphQLList) return []
+      throw new Error('Unauthorized')
+    }
+
+    return next()
+  },
+)
