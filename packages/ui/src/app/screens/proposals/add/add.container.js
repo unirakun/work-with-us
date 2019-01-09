ab6eccb8e766a8c46d38d97b93e5d708d1f7223f
Fabien JUIF <fabien.juif@gmail.com>
2019-01-09 09:47:51 +0100

    :fire: proposals

--- packages/ui/src/app/screens/proposals/add/add.container.js
+++
@@ -1,8 +0,0 @@
-import { graphql } from '@work-with-us/ui-hoc'
-import Component from './add'
-
-const mutation = `mutation AddProposal($input : InputProposal!) {
-  addProposal(input: $input)
-}`
-
-export default graphql(mutation)(Component)
